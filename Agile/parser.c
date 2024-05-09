187767c4e3558b7bcd4e46e15ae2ff95dff76744
luid <eng.limaluis@gmail.com>
2024-05-09 17:04:03 -0300

    feat: parser feat: maps

--- Agile/parser.c
+++ Agile/parser.c
@@ -16,90 +16,68 @@ int main(void){
 		while(matrix[i][j++] != '\0');
 		maxColumns = maxColumns > j ? maxColumns : j;}
 
+  int lenColumns[p-1];
+  for(int i=0;i<p-1;i++) lenColumns[i] = strlen(matrix[i]);
+
 	for(int i=0;i<p-1;i++)
-		for(int j=0;j<maxColumns/2;j++)
-			fprintf( problem, "loc%d%d ", i,j);
+		for(int j=0;j<lenColumns[i]/2;j++)
+			fprintf( problem, "loc%d-%d ", i,j);
 	fprintf( problem, " - location)\n\t (:init\n\t");
 
 	for(int i=0;i<p-1;i++){
 		int h=0;
 		int k=1;
-		for(int j=0;j<maxColumns;j++){
+    fprintf(problem, "\n");
+		for(int j=0;j<lenColumns[i];j++){
 			if(matrix[i][k] == 'W'){
-				//fprintf(problem, "(button l%d c%d)\n\t",i,j);
-				fprintf(problem, "(white loc%d%d) ",i,j);}
+				fprintf(problem, "(white loc%d-%d) ",i,j);}
 			if(matrix[i][k] == 'R'){
-				//fprintf(problem, "(button l%d c%d)\n\t",i,j);
-				fprintf(problem, "(red loc%d%d) ",i,j);}
+				fprintf(problem, "(red loc%d-%d) ",i,j);}
 			if(matrix[i][k] == 'B'){
-				//fprintf(problem, "(button l%d c%d)\n\t",i,j);
-				fprintf(problem, "(blue loc%d%d) ",i,j);}
+				fprintf(problem, "(blue loc%d-%d) ",i,j);}
 			if(matrix[i][k] == 'G'){
-				//fprintf(problem, "(button lox%dc%d)\n\t",i,j);
-				fprintf(problem, "(green loc%d%d) ",i,j);}
+				fprintf(problem, "(green loc%d-%d) ",i,j);}
 
-			//if(matrix[i][h] == '-')
-				//fprintf(problem, "(normal loc%d%d)\n\t",i,j);
 			if(matrix[i][h] == '*')
-				fprintf(problem, "(broken1 loc%d%d) ",i,j);
+				fprintf(problem, "(broken1 loc%d-%d) ",i,j);
 			if(matrix[i][h] == '_')
-				fprintf(problem, "(broken2 loc%d%d) ",i,j);
+				fprintf(problem, "(broken2 loc%d-%d) ",i,j);
 			if(matrix[i][h] == '|')
-				fprintf(problem, "(broken3 loc%d%d) ",i,j);
+				fprintf(problem, "(broken3 loc%d-%d) ",i,j);
 			if(matrix[i][h] == '#')
-				fprintf(problem, "(broken4 loc%d%d) ",i,j);
+				fprintf(problem, "(broken4 loc%d-%d) ",i,j);
 			h+=2; k+=2; 
 		}
 	}
-    fprintf(problem, "\n");
-    fprintf(problem, "\n");
-
-  int lenColumns[p-1];
-  for(int i=0;i<p-1;i++) lenColumns[i] = strlen(matrix[i]);
-
+fprintf(problem, "\n");
 for(int i=0;i<p-1;i++){
   fprintf(problem, "\n");
   for(int ii=0;ii<lenColumns[i]/2;ii++){
     fprintf(problem, "\n");
     for(int j=0;j < lenColumns[i]/2;j++){
-      if(ii != j) fprintf(problem, "(line loc%d%d loc%d%d) ", i,ii,i,j);
+      if(ii != j) fprintf(problem, "(line loc%d-%d loc%d-%d) ", i,ii,i,j);
     }
   }
 }
-  fprintf(problem, "\n");
-  fprintf(problem, "\n");
-
+fprintf(problem, "\n");
 for(int i=0;i<maxColumns/2;i++){
   fprintf(problem, "\n");
   for(int ii=0;ii<p-1;ii++){
   fprintf(problem, "\n");
     for(int j=0;j<p-1;j++){
-      //if(matrix[j][i] != 0){
-        if(j != ii) fprintf(problem, "(column loc%d%d loc%d%d) ",ii,i,j,i); 
-      //}
+        if(j != ii) {
+          if(i <= lenColumns[ii]/2-1 && i <= lenColumns[j]/2-1) fprintf(problem, "(column loc%d-%d loc%d-%d) ",ii,i,j,i); 
+        }
+    }
     }
   }
-}
-
 fprintf(problem, ")\n\t(:goal (and\n\t");
-	for(int i=0;i<p;i++){
-		int h=0;
-		int k=1;
-		for(int j=0;j<maxColumns;j++){
-			if(matrix[i][k] == 'W')
-				fprintf(problem, "(white loc%d%d) ",i,j);
-			if(matrix[i][k] == 'R')
-				fprintf(problem, "(white loc%d%d) ",i,j);
-			if(matrix[i][k] == 'B')
-				fprintf(problem, "(white loc%d%d) ",i,j);
-			if(matrix[i][k] == 'G')
-				fprintf(problem, "(white loc%d%d) ",i,j);
-
-			h+=2; k+=2; 
-		}
-	}
+  for(int i=0;i<p-1;i++){
+    fprintf(problem, "\n");
+    for(int j=0;j<lenColumns[i]/2;j++)
+      fprintf(problem, "(white loc%d-%d) ",i,j); }
 	fprintf(problem, ")))\n");
-	fprintf(domain," (define (domain lightsOut) (:requirements :strips :typing :equality) (:types location - object) (:predicates (red  ?yx - location) (green  ?yx - location) (blue  ?yx - location) (white  ?yx - location) (line ?yx ?wz - location) (column ?yx ?wz - location) (broken1  ?yx - location) (broken2  ?yx - location) (broken3  ?yx - location) (broken4  ?yx - location)) (:action click :parameters( ?yx - location) :precondition ( and) :effect (and (when (not (broken1 ?yx)) (and (when (red ?yx) (and (not (red ?yx)) (green ?yx))) (when (green ?yx) (and (not (green ?yx)) (blue ?yx))) (when (blue ?yx) (and (not (blue ?yx)) (white ?yx))) (when (white ?yx) (and (not (white ?yx)) (red ?yx))))) (forall (?wz - location) (and (when (and (line ?yx ?wz) (not (= ?yx ?wz)) (not (broken3 ?wz)) (not (broken4 ?wz))) (and (when (red ?wz) (and (not (red ?wz)) (green ?wz))) (when (green ?wz) (and (not (green ?wz)) (blue ?wz))) (when (blue ?wz) (and (not (blue ?wz)) (white ?wz))) (when (white ?wz) (and (not (white ?wz)) (red ?wz))))) (when (and (column ?yx ?wz) (not (= ?yx ?wz)) (not (broken2 ?wz)) (not (broken4 ?wz))) (and (when (red ?wz) (and (not (red ?wz)) (green ?wz))) (when (green ?wz) (and (not (green ?wz)) (blue ?wz))) (when (blue ?wz) (and (not (blue ?wz)) (white ?wz))) (when (white ?wz) (and (not (white ?wz)) (red ?wz))))))))))");
+	fprintf(domain," (define (domain lightsOut) (:requirements :strips :typing :equality) (:types location - object) (:predicates (red  ?y-x - location) (green  ?y-x - location) (blue  ?y-x - location) (white  ?y-x - location) (line ?y-x ?w-z - location) (column ?y-x ?w-z - location) (broken1  ?y-x - location) (broken2  ?y-x - location) (broken3  ?y-x - location) (broken4  ?y-x - location)) (:action click :parameters( ?y-x - location) :precondition ( and) :effect (and (when (not (broken1 ?y-x)) (and (when (red ?y-x) (and (not (red ?y-x)) (green ?y-x))) (when (green ?y-x) (and (not (green ?y-x)) (blue ?y-x))) (when (blue ?y-x) (and (not (blue ?y-x)) (white ?y-x))) (when (white ?y-x) (and (not (white ?y-x)) (red ?y-x))))) (forall (?w-z - location) (and (when (and (line ?y-x ?w-z) (not (= ?y-x ?w-z)) (not (broken3 ?w-z)) (not (broken4 ?w-z))) (and (when (red ?w-z) (and (not (red ?w-z)) (green ?w-z))) (when (green ?w-z) (and (not (green ?w-z)) (blue ?w-z))) (when (blue ?w-z) (and (not (blue ?w-z)) (white ?w-z))) (when (white ?w-z) (and (not (white ?w-z)) (red ?w-z))))) (when (and (column ?y-x ?w-z) (not (= ?y-x ?w-z)) (not (broken2 ?w-z)) (not (broken4 ?w-z))) (and (when (red ?w-z) (and (not (red ?w-z)) (green ?w-z))) (when (green ?w-z) (and (not (green ?w-z)) (blue ?w-z))) (when (blue ?w-z) (and (not (blue ?w-z)) (white ?w-z))) (when (white ?w-z) (and (not (white ?w-z)) (red ?w-z))))))))))");
 	fclose(domain);
 	fclose(problem);
 
@@ -107,39 +85,11 @@ fprintf(problem, ")\n\t(:goal (and\n\t");
 	system("/tmp/dir/software/planners/madagascar/M -Q domain.pddl problem.pddl | grep -i click > out");
 	//to run on chococino
 	//system("/home/software/planners/madagascar/M -Q domain.pddl problem.pddl | grep -i click > out");
-	system("grep -Eo '(click [a-zA-Z][[a-zA-Z][a-zA-Z]0-9][0-9])' < out > out2");
-        system("sed 's/^[0-9]* : (click loc\\([0-9]\\)\\([0-9]\\))/\\(click \\1 \\2\\);/g' out > outFile");
-
-	
-
-
-	/*
-
-	FILE *inputFile = fopen("out2", "r");
-	FILE *outFile = fopen("outFile", "w");
-	char line[100];
-	int l_num,c_num;
-	 while (fgets(line, sizeof(line), inputFile) != NULL){
-		if (sscanf(line, "click loc%d%d", &l_num, &c_num) == 2){
-		    fprintf(outFile, "(click %d%d);", l_num, c_num);}
-		 else { fprintf(outFile, "%s", line);}}
-	 fclose(inputFile);
-	 fclose(outFile);
-	 */
-	
-
+	//system("/home/software/planners/downward/fast-downward.py --alias lama-first domain9.pddl problem.pddl | grep -i click > out");
+	system("grep -Eo '(click [a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[0-9]+)' < out > out2");
+	system("sed 's/^[0-9]* : (click loc\\([0-9]*\\)-\\([0-9]*\\))/\\(click \\1 \\2\\);/' out > outFile");
 	system("sed '$ s/.$//' outFile > outFile2");
-	//int lines = system("wc -l < outFile2 > lines");
-
-    	FILE *pipe; char buffer[200]; int line;
-
-    	pipe = popen("sed -n '$=' outFile2", "r");
-
-   	if (fgets(buffer, 200, pipe) != NULL) line = atoi(buffer);
-	pclose(pipe);
-
-	if(line < 30) system("cat outFile2");
-	else while(1);
+    system("cat outFile2");
 
 	return 0;
 }
